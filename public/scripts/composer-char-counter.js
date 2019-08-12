5620048b0fddfd0c525db6e40e4c8f4c51abe267
Annie <anniekao@posteo.net>
2019-08-12 17:00:56 -0400

    Updated char counter to modified counter class

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
+  
   $('textarea').keyup(function(event) {
     let val = this.value.length;
     let count = 0;
     if (count < 140 && count > 0 && event.keyCode === 8) {
       count++;
+      $(".counter").text(count);
     } else {
       count = 140 - val;
+      $(".counter").text(count);
     }
   });
 
