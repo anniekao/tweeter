ac93b66ddd8ea513c984b332278e0bb1de680da3
Annie <anniekao@posteo.net>
2019-08-13 13:47:23 -0400

    Fixed code to use only jquery

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,23 +1,39 @@
+// Current jquery improved code
+
 $(document).ready(function() {
-  let count = 0;
+  $('textarea').keyup(function() {
+    const length = $(this).val().length;
+    const charsRemaining = 140 - length;
+    
+    $('.counter').text(charsRemaining);
 
-  $('textarea').keyup(function(event) {
-    const span = this.closest("form").children[3];
-    let val = this.value.length;
-
-    if (count < 140 && event.keyCode === 8) {
-      count++;
-      span.innerText = count;
+    // // add or remove class depending on counter value
+    if (charsRemaining < 0) {
+      $('.counter').addClass("warning");
     } else {
-      count = 140 - val;
-      span.innerText = count;
-    }
-
-    if (count < 0) {
-      span.style.color = "red";
-    } else {
-      span.style.color = "#545149";
+      $('.counter').removeClass("warning");
     }
   });
-
 });
+
+
+
+// Previous solution that did not completely use jquery...
+
+// $(document).ready(function() {
+
+//   $('textarea').keyup(function() {
+//     const span = this.closest("form").children[3];
+//     let val = $('textarea').val().length;;
+//     const charsRemaining = 140 - val;
+
+//     span.innerText = charsRemaining;
+
+//     if (charsRemaining < 0) {
+//       //span.style.color = "red";
+//     } else {
+//       //span.style.color = "#545149";
+//     }
+//   });
+
+// });
