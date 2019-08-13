fb7b3fe2d93e174635ad2c539cd09c72b20e2147
Annie <anniekao@posteo.net>
2019-08-13 17:21:47 -0400

    Added script/Ajax POST request to index.html

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -28,26 +28,3 @@ const createTweetElement = (tweet) => {
 
   return $tweet;
 };
-
-
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-    "created_at": 1461116232227
- }
-
-const $tweet = createTweetElement(tweetData);
-
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-
-$(()=> {
-  $("#tweets-container").append($tweet);
-})
